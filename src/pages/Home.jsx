f85f16538bb1de43484683b3e026ffef7ac86c26
sarvar <sarvar@gmail.com>
2024-12-08 20:38:56 +0500

    12345

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { axiosInstance } from "../utils/axiosInstance";
+import ProductsContainer from "../components/ProductsContainer";
+// loader
+export const loader = async () => {
+  const req = await axiosInstance.get("/products");
+  const products = req.data.products;
+  return { products };
+};
+function Home() {
+  return <ProductsContainer />;
+}
+export default Home;
